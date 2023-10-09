8b358670af692314613179340e4dfa686b3a0ab5
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-09 14:52:29 +0200

    create index file

--- index.php
+++ index.php
@@ -8,6 +8,19 @@
 </head>
 
 <body>
+
+    <form action="script.php">
+        <div>
+            <label for="paragraph">Inserisci il testo</label>
+            <input type="text" name="paragraph" id="paragraph">
+        </div>
+        <div>
+            <label for="badword">Inserisci la parola da censurare</label>
+            <input type="text" name="badword" id="badword">
+        </div>
+        <button type="submit">invia</button>
+    </form>
+
 </body>
 
 </html>
